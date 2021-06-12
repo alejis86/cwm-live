65914b434d0cc979ced0e5bf7dfc1c29442ffb9f
Mayra <mayra@iMac-de-Mayra.local>
2021-06-12 07:34:12 -0700

    Updates

---
+++ precache-manifest.0005dbb4e59edd522f4690a88505a74e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78ac9f8d7b629e3e1b1c",
+    "url": "/cwm-live/static/css/main.9771bcde.chunk.css"
+  },
+  {
+    "revision": "78ac9f8d7b629e3e1b1c",
+    "url": "/cwm-live/static/js/main.78ac9f8d.chunk.js"
+  },
+  {
+    "revision": "ac314d1e264f3abef936",
+    "url": "/cwm-live/static/css/1.0285b502.chunk.css"
+  },
+  {
+    "revision": "ac314d1e264f3abef936",
+    "url": "/cwm-live/static/js/1.ac314d1e.chunk.js"
+  },
+  {
+    "revision": "9bd803f02ee66762c9f1",
+    "url": "/cwm-live/static/js/runtime~main.9bd803f0.js"
+  },
+  {
+    "revision": "582113f5e7ffc6c720d829af9212f763",
+    "url": "/cwm-live/static/media/logo.582113f5.png"
+  },
+  {
+    "revision": "98479e817aaac835ec00e7c79e08805a",
+    "url": "/cwm-live/index.html"
+  }
+];
